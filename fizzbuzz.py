14fa6c26f0696d84c838f1da265dcb1c6c61250c
sushm <sushmamurakonda@gmail.com>
2024-07-30 21:26:09 -0400

    added the code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
 # add your code here
-
+for x in range(1,101):
+    if x%3 == 0 and x%5 == 0:
+        print("fuzzbuzz")
+    elif x%3 == 0:
+        print ("fizz")
+    elif x%5 == 0:
+        print ("buzz")
+    else:
+        print(x)
